239d7716b501c0cbff5e4ee00066f2277fc84585
KadirbekSharau <sharaukadr2001@gmail.com>
2023-01-27 18:04:04 +0600

    database initialization

--- challenge-app/src/main/kotlin/org/light/challenge/app/App.kt
+++ challenge-app/src/main/kotlin/org/light/challenge/app/App.kt
@@ -1,11 +1,19 @@
 package org.light.challenge.app
 
 import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
 
+import org.light.challenge.data.Company
+import org.light.challenge.data.Department
+import org.light.challenge.data.Employee
+import org.light.challenge.data.Invoice
+import org.light.challenge.data.Rule
+import org.light.challenge.data.Workflow
+import org.light.challenge.data.Approval
 fun main() {
-
-    // in-memory DB
-    val db = Database.connect("jdbc:sqlite::memory:test?mode=memory&cache=shared", "org.sqlite.JDBC")
-
-    // TODO: placeholder - start the program here
+    val db = Database.connect("jdbc:sqlite::memory:", "org.sqlite.JDBC")
+    transaction{
+        SchemaUtils.create(Company, Department, Employee, Invoice, Rule, Workflow, Approval)   
+    }
 }
--- challenge-data/src/main/kotlin/org/light/challenge/data/tables.kt
+++ challenge-data/src/main/kotlin/org/light/challenge/data/tables.kt
@@ -1,3 +1,57 @@
 package org.light.challenge.data
 
+import org.jetbrains.exposed.sql.Table
+
 // TODO: placeholder - DB tables here
+object Company : Table() {
+    val id = integer("id").autoIncrement().primaryKey()
+    val name = varchar("name", 50)
+    val department_id = (integer("department_id") references Department.id)
+}
+
+object Department : Table() {
+    val id = integer("id").autoIncrement().primaryKey()
+    val name = varchar("name", 50)
+}
+
+object Employee : Table() {
+    val id = integer("id").autoIncrement().primaryKey()
+    val name = varchar("name", 50)
+    val is_manager = bool("is_manager")
+    val company_id = (integer("company_id") references Company.id)
+    val department_id = (integer("department_id") references Department.id) 
+}
+
+object Invoice : Table() {
+    val id = integer("id").autoIncrement().primaryKey()
+    val amount = decimal("amount", precision = 10, scale = 2)
+    val is_manager_approval_required = bool("is_manager_approval_required")
+    val related_department_id = (integer("related_department_id") references Department.id)
+    val company_id = (integer("company_id") references Company.id)
+    val workflow_id = (integer("workflow_id") references Workflow.id)
+}
+
+object Rule : Table() {
+    val id = integer("id").autoIncrement().primaryKey()
+    val min_amount = decimal("min_amount", precision = 10, scale = 2)
+    val max_amount = decimal("max_amount", precision = 10, scale = 2)
+    val is_manager_approval_required = bool("is_manager_approval_required")
+    val related_department_id = (integer("related_department_id") references Department.id)
+    val destination_department_id = (integer("destination_department_id") references Department.id)
+    val workflow_id = (integer("workflow_id") references Workflow.id)
+}
+
+object Workflow : Table() {
+    val id = integer("id").autoIncrement().primaryKey()
+    val name = varchar("name", 50)
+    val company_id = (integer("company_id") references Company.id)
+}
+
+object Approval : Table() {
+    val id = integer("id").autoIncrement().primaryKey()
+    val invoice_id = (integer("invoice_id") references Invoice.id)
+    val employee_id = (integer("employee_id") references Employee.id)
+    val status_approved = bool("status_approved")
+    val date = date("date")
+    val approval_method = integer("approval_method")
+}
